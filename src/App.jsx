f6d298da671ac176fd09e75ff0f995c246c50828
MaryKaratu <Karatumary85@gmail.com>
2025-05-06 15:58:50 +0100

    Props Syntax

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import reactImg from "./assets/react-core-concepts.png";
-import componentImg from "./assets/components.png";
+import { CORE_CONCEPTS } from "./data.js";
 
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
@@ -22,12 +22,21 @@ function Header() {
   );
 }
 
-function CoreConcept(props) {
+// function CoreConcept(props) {
+//   return (
+//     <li>
+//       <img src={props.image} alt={props.title} />
+//       <h3>{props.title}</h3>
+//       <p>{props.description}</p>
+//     </li>
+//   );
+// }
+function CoreConcept({ image, title, description }) {
   return (
     <li>
-      <img src={props.img} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
     </li>
   );
 }
@@ -42,18 +51,13 @@ function App() {
           <h2>Time to get started!</h2>
           <ul>
             <CoreConcept
-              title="Component"
-              description=""
-              The
-              core
-              UI
-              building
-              block
-              img={componentImg}
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
             />
-            <CoreConcept />
-            <CoreConcept />
-            <CoreConcept />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
       </main>
